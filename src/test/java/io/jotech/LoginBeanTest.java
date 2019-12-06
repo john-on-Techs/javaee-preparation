fef8dcfefef615ce94c5277bd8b0e8f8b3b0adb9
john-on-Techs <gavanayeye@gmail.com>
2019-12-06 16:46:25 +0300

    Add wildfly deploy plugin and make all tests pass

--- src/test/java/io/jotech/LoginBeanTest.java
+++ src/test/java/io/jotech/LoginBeanTest.java
@@ -29,7 +29,7 @@ public class LoginBeanTest {
     @Test
     public void login() {
         String loginResponse = loginBean.login("John");
-        Assert.assertEquals("Hello, Johns", loginResponse);
+        Assert.assertEquals("Hello, John", loginResponse);
 
     }
 
